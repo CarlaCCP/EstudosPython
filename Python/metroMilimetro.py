789a098652cba87e140d1050305d86b2e05aefee
Carla Cristina <carldepaula230@gmail.com>
2020-10-16 21:15:34 -0300

    wip

---
+++ Python/metroMilimetro.py
@@ -0,0 +1,3 @@
+metros = int(input("Digite a quantidade de metros: "))
+milimetro = metros * 1000
+print(metros , "metros convertidos para milimetro é " , milimetro)
---
+++ Python/numDobro.py
@@ -0,0 +1,4 @@
+n1 = int(input("Digite um número: "))
+print("O dobro de " , n1 , " é " , n1*2)
+print("O triplo de " , n1 , " é " , n1 **3)
+print("O quadrado de ", n1 , " é " , n1**2) 
